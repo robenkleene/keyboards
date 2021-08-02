07277e83d7e63f54f95f8fde811bcbf34cedc838
Roben Kleene <contact@robenkleene.com>
2021-08-02 06:33:25 -0400

    Revert "Tweak layout"
    This reverts commit cffce683ad05ce7e8451d110ca30a2c7feecc53a.

--- ergodox_ez/robenkleene_macos2/keymap.c
+++ ergodox_ez/robenkleene_macos2/keymap.c
@@ -11,11 +11,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  BS    |
+ * |  `     |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |  =   |   Y  |   U  |   I  |   O  |   P  |   "    |
+ * | ESC    |   Q  |   W  |   E  |   R  |   T  |      |           |  =   |   Y  |   U  |   I  |   O  |   P  |  BS    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Return
+ * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  '     |
  * |--------+------+------+------+------+------|      |           |  -   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
 
 // Left Keyboard
-KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
+KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
 KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
-KC_CAPS,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
+KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
 KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
 TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
 
@@ -47,8 +47,8 @@ KC_HOME,
 KC_SPC,   KC_ENT,    KC_END,
 
 // Right Keyboard
-KC_NO,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
-KC_EQL,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+KC_NO,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,
+KC_EQL,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
 KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
 KC_MINS,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
 MO(FCTN),  KC_RGUI,  KC_RALT,  KC_LCTL,  KC_ENT,
