dacf2a24279b198129651f0ab617082b8e86fb9d
Roben Kleene <contact@robenkleene.com>
2021-08-22 14:52:48 -0400

    Swap caps and numb

--- ergodox_ez/robenkleene_macos2/keymap.c
+++ ergodox_ez/robenkleene_macos2/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
 
 // Left Keyboard
-KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
-KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
-KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
-KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
-KC_CAPS,  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
+KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
+KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
+KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
+KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
+TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
 
 // Left Cluster
 KC_LGUI,  MO(FCTN),
@@ -90,7 +90,7 @@ KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,
 KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_BRIU,
 MO(UTIL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
 KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_BRID,
-TG(NUMB),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_CAPS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster
 KC_TRNS,  KC_TRNS,
