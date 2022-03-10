df01669da90760edab980439d92e7a06005a9b27
Roben Kleene <contact@robenkleene.com>
2022-03-10 17:40:26 -0500

    Tweak Moonlander

--- moonlander/robenkleene_macos/keymap.c
+++ moonlander/robenkleene_macos/keymap.c
@@ -39,7 +39,7 @@ KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,               
 KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,               KC_EQL,    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
 KC_ESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,  KC_NO,               KC_MINS,   KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
 KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,                       KC_N,      KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),  KC_HOME,                    KC_PGUP,   MO(FCTN),  KC_RGUI,  KC_RALT,  KC_LCTL,  KC_ENT,
+TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),  KC_HOME,                    KC_PGUP,   MO(FCTN), KC_RGUI,  KC_RALT,  KC_LCTL,  KC_ENT,
 
 // Left Cluster                                                                 // Right Cluster
 KC_SPC,   KC_ENT,    KC_END,                                                    KC_PGDN,   KC_ENT,   KC_SPC
@@ -74,8 +74,8 @@ KC_SPC,   KC_ENT,    KC_END,                                                    
 // Left Keyboard                                                                // Right Keyboard
 KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_TRNS,            KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_TRNS,
 KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,    KC_PAUS,            KC_VOLU,  KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_BSLS,
-MO(UTIL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                       KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_TRNS,
-KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_SLCK,            KC_VOLD,  KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,   KC_UP,    KC_TRNS,
+MO(UTIL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_SLCK,            KC_VOLD,  KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,   KC_RBRC,   KC_TRNS,
+KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,                      KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_UP,     KC_TRNS,
 KC_CAPS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,   KC_RIGHT,
 
 // Left Cluster                                                                 // Right Cluster
@@ -89,9 +89,9 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    
 // Left Keyboard                                                                // Right Keyboard
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS, 
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SLCK,  KC_PAUS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_MUTE,  KC_MPRV,  KC_MNXT,   KC_PGUP,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS, KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SLCK,  KC_PAUS,  KC_TRNS,                      KC_TRNS,  KC_MUTE,  KC_MPRV,  KC_MNXT,  KC_PGUP,   KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,   KC_END,
 
 // Left Cluster                                                                 // Right Cluster
 KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    KC_TRNS,  KC_TRNS,  KC_TRNS
@@ -105,9 +105,9 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    
 // Left Keyboard                                                                // Right Keyboard
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  RESET,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
 
 // Left Cluster                                                                 // Right Cluster
 KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    KC_TRNS,  KC_TRNS,  KC_TRNS
@@ -120,8 +120,8 @@ KC_TRNS,  KC_TRNS,  KC_TRNS,                                                    
 // Left Keyboard                                                                // Right Keyboard
 KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
 KC_TRNS,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,           KC_TRNS,  KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_TRNS,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 TG(NUMB),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster                                                                 // Right Cluster
@@ -136,7 +136,12 @@ void keyboard_post_init_user(void) {
 }
 
 void rgb_matrix_indicators_user(void) {
+  ML_LED_1(0);
   ML_LED_2(0);
+  ML_LED_3(0);
+  ML_LED_4(0);
+  ML_LED_5(0);
+  ML_LED_6(0);
   switch (biton32(layer_state)) {
     case BASE:
       rgb_matrix_set_color_all(17, 25, 17);
@@ -160,8 +165,8 @@ void rgb_matrix_indicators_user(void) {
       break;
   }
   if (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) {
-    ML_LED_1(1);
+    ML_LED_3(1);
   } else {
-    ML_LED_1(0);
+    ML_LED_3(0);
   }
 }
