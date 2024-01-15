2bb50bc2ac3bf1cd521b5015da1817ae4bc003f1
Roben Kleene <contact@robenkleene.com>
2024-01-15 20:57:58 +0000

    Remove old file

--- configure_path.fish
+++
@@ -1,7 +0,0 @@
-# source configure_path.fish
-if test -e /usr/local/opt/avr-gcc@8/bin
-    set -U fish_user_paths /usr/local/opt/avr-gcc@8/bin $fish_user_paths
-end
-if test -e /usr/local/opt/arm-gcc-bin@8/bin
-    set -U fish_user_paths /usr/local/opt/arm-gcc-bin@8/bin $fish_user_paths
-end
