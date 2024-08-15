9ff333a622c594c1e27f102c0e43b23943c64fba
Roben Kleene <contact@robenkleene.com>
2024-08-15 10:11:53 +0000

    Fix Voyagor num

--- voyager/robenkleene_macos/keymap.c
+++ voyager/robenkleene_macos/keymap.c
@@ -26,11 +26,11 @@ KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  FCTN,  KC_SPC,  KC_SPC,  FCTN,  KC_RGUI,
 KC_SPC,   KC_ESC,   KC_ENT,   KC_SPC
 ),
 [_FCTN] = LAYOUT(
-KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,
-KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_TRNS,
-KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_UP,    KC_TRNS,
-UTIL,     KC_TRNS,  KC_TRNS,  KC_TRNS,  FCT2,    KC_TRNS,  KC_TRNS,  FCT2,     KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,
-KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,    KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,
+TG(_NUMB),  KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,    KC_F6,    KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_TRNS,
+KC_TRNS,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,   KC_F12,   KC_MPLY,  KC_VOLD,  KC_VOLU,  KC_UP,    KC_TRNS,
+UTIL,       KC_TRNS,  KC_TRNS,  KC_TRNS,  FCT2,    KC_TRNS,  KC_TRNS,  FCT2,     KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,
+KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS
 ),
 [_FCT2] = LAYOUT(
 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
