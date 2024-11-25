ca98b1350cc3a98d9b3306e7734f8a05917d7360
Roben Kleene <contact@robenkleene.com>
2024-11-25 00:51:58 +0000

    Swap space

--- iris/robenkleene_macos/keymap.c
+++ iris/robenkleene_macos/keymap.c
@@ -22,7 +22,7 @@ KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,       KC_T,       KC_Y,   KC_U,   
 KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,       KC_G,       KC_H,   KC_J,   KC_K,       KC_L,       KC_SCLN,  KC_QUOT,
 KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,       KC_N,   KC_M,   KC_COMMA,   KC_DOT,     KC_SLSH,  KC_RSFT,
 KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  MO(_FCTN),  TG(_NUMB),  KC_NO,  KC_NO,  TG(_FCTN),  MO(_FCTN),  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT,
-KC_SPC,   KC_ESC,   KC_NO,    KC_NO,    KC_ENT,     KC_SPC
+KC_NO,    KC_SPC,   KC_ESC,   KC_ENT,   KC_SPC,     KC_NO
 ),
 [_FCTN] = LAYOUT(
 KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,       KC_5,     KC_6,     KC_7,             KC_8,       KC_9,     KC_0,     KC_BSLS,
