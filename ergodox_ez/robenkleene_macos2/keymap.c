5f81dccb8a69531f41625bc1020e9fc11558ba47
Roben Kleene <contact@robenkleene.com>
2025-02-24 16:26:10 +0000

    Clean up and keypad enter

--- ergodox_ez/robenkleene_macos2/keymap.c
+++ ergodox_ez/robenkleene_macos2/keymap.c
@@ -15,23 +15,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,  KC_NO,
 KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,  KC_NO,
 KC_CAPS,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,
-KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  TG(NUMB),
+KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,  KC_NO,
 TG(NUMB),  KC_LCTL,  KC_LALT,  KC_LGUI,  MO(FCTN),
 
 // Left Cluster
-TG(NUMB),  KC_NO,
+KC_NO,   KC_NO,
 KC_NO,
-KC_SPC,    KC_ESC,  KC_NO,
+KC_SPC,  KC_ESC,  KC_NO,
 
 // Right Keyboard
-KC_NO,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_NO,
-KC_NO,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-TG(FCTN),  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+KC_NO,     KC_6,     KC_7,     KC_8,     KC_9,      KC_0,     KC_NO,
+KC_NO,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,     KC_BSPC,
+KC_H,      KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
+KC_NO,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,
 MO(FCTN),  KC_RGUI,  KC_RALT,  KC_LCTL,  TG(FCTN),
 
 // Right Cluster
-KC_NO,  TG(FCTN),
+KC_NO,  KC_NO,
 KC_NO,
 KC_NO,  KC_ENT,    KC_SPC
     ),
@@ -41,11 +41,11 @@ KC_NO,  KC_ENT,    KC_SPC
 
 // Left Keyboard
 
-KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,   KC_TRNS,
-KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,    KC_TRNS,
-TG(NUMB),  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,
-KC_TRNS,   KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,  KC_TRNS,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(FCT2),
+KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,   KC_TRNS,
+KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,      KC_5,    KC_TRNS,
+KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,
+KC_TRNS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  MO(FCT2),
 
 // Left Cluster
 
@@ -93,7 +93,7 @@ KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END,   KC_TRNS,
 // Right Cluster
 KC_TRNS,  KC_TRNS,
 KC_TRNS,
-KC_TRNS,  KC_TRNS,  KC_TRNS
+KC_TRNS,  KC_PENT,  KC_TRNS
 
 ),
 
@@ -130,11 +130,11 @@ KC_TRNS,  KC_TRNS,  KC_TRNS
 [NUMB] = LAYOUT_ergodox(
 
 // Left Keyboard
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-KC_TRNS,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-TG(NUMB),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
 
 // Left Cluster
 KC_TRNS,  KC_TRNS,
@@ -169,7 +169,7 @@ bool rgb_matrix_indicators_user(void) {
 
   if (host_keyboard_led_state().caps_lock) {
     ergodox_right_led_3_on();
-    rgb_matrix_set_color_all(0, 0, 25);
+    rgb_matrix_set_color_all(0, 0, 255);
     return false;
   } else {
     ergodox_right_led_3_off();
@@ -180,18 +180,18 @@ bool rgb_matrix_indicators_user(void) {
       rgb_matrix_set_color_all(17, 25, 17);
       break;
     case FCTN:
-      rgb_matrix_set_color_all(25, 0, 25);
+      rgb_matrix_set_color_all(255, 0, 255);
       break;
     case FCT2:
-      rgb_matrix_set_color_all(0, 25, 25);
+      rgb_matrix_set_color_all(0, 255, 255);
       break;
     case NUMB:
       ergodox_right_led_2_on();
-      rgb_matrix_set_color_all(0, 25, 0);
+      rgb_matrix_set_color_all(0, 255, 0);
       break;
     case UTIL:
       ergodox_right_led_1_on();
-      rgb_matrix_set_color_all(25, 0, 0);
+      rgb_matrix_set_color_all(255, 0, 0);
       break;
     default:
       if (rgb_matrix_get_flags() == LED_FLAG_NONE)
